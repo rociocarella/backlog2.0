fea22745608dffb7c2770adfc1959ccd5483b281
japadedog <mejias.llanos11@gmail.com>
2020-01-30 15:04:52 -0300

    le menu

--- src/java/controlador/ServletLogin.java
+++ src/java/controlador/ServletLogin.java
@@ -32,7 +32,7 @@ public class ServletLogin extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-response.sendRedirect("Login.jsp");
+response.sendRedirect("login.jsp");
         }
     }
 
---
+++ src/java/modelo/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author japa
+ */
+public class Usuario {
+    
+    private int id_usuario;
+    private String nombre;
+    private String password;
+    private String token_csrf;
+    private String ultima_conexion;
+    private String correo_electronico;
+    private int id_tipo_usuario;
+    
+}
